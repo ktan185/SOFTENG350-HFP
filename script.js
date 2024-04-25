6ff94fe10b53f4fd94d4b455626828ba5be9a10b
tangkay <tangkay10k@gmail.com>
2024-04-25 18:33:07 +1200

    fix: Added alt for accessibility

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // Function to open the sign-up form
 function openForm() {
-  var signUpForm = document.getElementById('signUpForm');
+  var signUpForm = document.getElementById('form-ct');
   signUpForm.style.display = 'block'; // Display the form
 
   var overlay = document.createElement('div');
   overlay.classList.add('overlay');
   document.body.appendChild(overlay);
-
 }
 
 // Function to close the sign-up form
 function closeForm() {
-  var signUpForm = document.getElementById('signUpForm');
+  var signUpForm = document.getElementById('form-ct');
   signUpForm.style.display = 'none';
 
   var overlay = document.querySelector('.overlay');
